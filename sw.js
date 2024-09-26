e2c2ebaff29443c804684967184087c8fcd4a7f8
benesse_Umemura_Yoshinori <70671547+benesse-umemura@users.noreply.github.com>
2024-09-26 14:49:52 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,9 @@
-// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
-
-// キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
+var CACHE_NAME = 'pwasample';
 var urlsToCache = [
-    '/pwa/',
-    '/pwa/css/style.css',
-    '/pwa/drawer.js'
+    '/apps/pwa/',
+    '/apps/pwa/index.html'
 ];
 
-// インストール処理
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches
@@ -19,7 +14,6 @@ self.addEventListener('install', function(event) {
     );
 });
 
-// リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches
